326ac80a9df1e960c94403c149c539e1dd65e0d7
ScharxiDev <scharxidev@outlook.de>
2022-06-08 14:24:42 +0200

    Implemented Config Service 💾

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
+  imports: [ConfigModule.forRoot({
+    envFilePath: ".env",
+    isGlobal: true,
+  })]
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger('NestApplication');
+
+  const configService = app.get(ConfigService);
 
   const config = new DocumentBuilder()
     .setTitle('Cats example')
@@ -14,6 +19,22 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  const port = configService.get<string>('PORT') || 3000;
+  if (port == 3000) {
+    logger.warn('PORT is not set in .env file, using default port 3000 instead');
+  }
+  if (!port) {
+    logger.error('PORT is not defined in the .env file!');
+    return;
+  }
+  if (isNaN(+port)) {
+    logger.error('PORT is not a valid number!');
+    return;
+  }
+
+  await app.listen(port).then(() => {
+    logger.log(`Application is running on http://localhost:${port}`);
+    logger.log(`View API Docs on http://localhost:${port}/api`);
+  });
 }
 bootstrap();
